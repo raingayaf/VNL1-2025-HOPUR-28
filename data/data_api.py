04b0373072a09853c91380f5fd02bca0057166eb
Thorsteinnk92 <thorsteinnk92@gmail.com>
2025-12-03 14:58:36 +0000

    match layer work

--- data/data_api.py
+++ data/data_api.py
@@ -1,3 +1,11 @@
+from matches_data import MatchesData
+
 class DataApi:
-    def __init__(self,): None
-pass
+    def __init__(self, matches_data: MatchesData):
+        self.matches_data = matches_data 
+
+    def read_all_matches(self) -> list[Matches]:
+        return self.matches_data.read_all()
+    
+    # TODO save_menu?
+
