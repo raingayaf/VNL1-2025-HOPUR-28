a665c19af417bcc4f5a4b6deda47c118346df728
katrinlif95 <katrinlif95@gmail.com>
2025-12-03 17:56:51 +0000

    Ennþá fleiri breytingar og lagfæringar á UI

--- ui/dummy.py
+++ ui/dummy.py
@@ -1,7 +1,7 @@
 WIDTH = 60
 
-def main_menu():
-    """Aðal valmyndin."""
+def display_main_menu():
+    """Display main menu options to user."""
     print('*' * WIDTH)
     print('E-SPORTS'.center(WIDTH))                                                          
     print('*' * WIDTH + '\n')
@@ -12,12 +12,12 @@ def main_menu():
     print('*' * WIDTH + '\n')
     # user_input = input('Sláðu inn númer aðgerðar: ')
 
-# main_menu() 
+# display_main_menu() 
 # --> Ef þið viljið sjá hvernig þetta prentast út 
 
 
-def menu_user():
-    """Fyrsta valmyndin hjá notanda."""
+def display_tournaments():
+    """Display all tournaments in the system to general users."""
     print('*' * WIDTH)
     print('E-SPORTS'.center(WIDTH))                                                          
     print('*' * WIDTH + '\n')
@@ -26,27 +26,72 @@ def menu_user():
     print('*' * WIDTH + '\n')
     # user_input = input('Sláðu inn númer móts: ')
 
-# menu_user() 
+# display_tournaments() 
 #--> Ef þið viljið sjá hvernig þetta prentast út 
 
 
-def menu_tournament():
-    """Notandi hefur valið mótið sem hann vill skoða og getur valið um að skoða dagskrá, stöðutöflu og keppnislið."""
+def display_tournament_menu():
+    """Display menu options for the selected tournament."""
     print('*' * WIDTH)
     print('E-SPORTS'.center(WIDTH))                                                          
     print('*' * WIDTH + '\n')
-    print('XXXX'.center(WIDTH) + '\n')         # XXXX á að vera heiti á móti
+    print('XXXX'.center(WIDTH) + '\n')   # XXXX á að vera heiti á móti
     print(' 1. Dagskrá')
     print(' 2. Stöðutafla')
     print(' 3. Keppnislið' + '\n')
     print('*' * WIDTH + '\n')
     # user_input = input('Sláðu inn númer aðgerðar: ')
 
-# menu_tournament() 
-# #--> Ef þið viljið sjá hvernig þetta prentast út
+# display_tournament_menu() 
+#--> Ef þið viljið sjá hvernig þetta prentast út
 
 
-def menu_organizer():
+def display_tournament_schedule():
+    """Display tournament schedule."""
+    print('*' * WIDTH)
+    print('E-SPORTS'.center(WIDTH))                                                          
+    print('*' * WIDTH + '\n')
+    print('Dagskrá'.center(WIDTH) + '\n')
+    # Hér mun dagskrá mótsins vera
+
+def display_tournament_scoreboard():
+    """Display tournament scoreboard."""
+    print('*' * WIDTH)
+    print('E-SPORTS'.center(WIDTH))                                                          
+    print('*' * WIDTH + '\n')
+    print('Stöðutafla'.center(WIDTH) + '\n')
+    # Hér mun stöðutafla mótsins vera
+
+
+def display_tournament_teams():
+    """Display tournament teams."""
+    print('*' * WIDTH)
+    print('E-SPORTS'.center(WIDTH))                                                          
+    print('*' * WIDTH + '\n')
+    print('Keppnislið'.center(WIDTH) + '\n')
+    # Hér mun listi yfir keppnislið mótsins vera
+    print('1. Team#1')   # dæmi
+    print('2. Team#2')
+    print('3. Team#3' + '\n')
+    print('*' * WIDTH + '\n')
+    # user_input = input('Sláðu inn númer liðs: ')
+
+
+def display_team_players():
+    """Display players on a selected team who participate/d in the tournament."""
+    print('*' * WIDTH)
+    print('E-SPORTS'.center(WIDTH))                                                          
+    print('*' * WIDTH + '\n')
+    print('XXXXX'.center(WIDTH) + '\n') # XXXXXX á að vera heiti á liði
+    # Hér á að koma listi yfir leikmenn liðsins
+    print('1. Player#1')   # dæmi
+    print('2. Player#2')
+    print('3. Player#3' + '\n')
+    print('*' * WIDTH + '\n')
+
+
+def display_organizer_menu():
+    """Display menu options to tournament organizer."""
     print('*' * WIDTH)
     print('E-SPORTS'.center(WIDTH))                                                          
     print('*' * WIDTH + '\n')
@@ -58,13 +103,17 @@ def menu_organizer():
     # user_input = input('Sláðu inn númer aðgerðar: ')
 
 
-
-
-def menu_team_captain():
+def display_captain_menu():
+    """Display menu options to team captain."""
     print('*' * WIDTH)
     print('E-SPORTS'.center(WIDTH))                                                          
     print('*' * WIDTH + '\n')
-                                           
+    print('Velkominn mótshaldari'.center(WIDTH) + '\n')
+    print(' 1. Skrá lið')
+    print(' 2. Skoða lið' + '\n')
+    print('*' * WIDTH + '\n')
+    # user_input = input('Sláðu inn númer aðgerðar: ')
+                                  
                                  
 
 
