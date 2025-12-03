f1c8e3d4f50e8962d4d7f25e9e24c95de3df19ff
raingayaf <rainmdot@gmail.com>
2025-12-03 10:42:45 +0000

    this

---
+++ models/models.py
@@ -0,0 +1,17 @@
+from datetime import date 
+
+class Tournament:
+        def __init__(self, name, startDate, endDate, venue, contactPerson, teams, matches):
+                pass 
+class Match:
+        def __init__(self, matchId, teamA, teamB, scheduledDate, serverId, result)
+                
+class Team:
+        def __innit__(self, name, teamId, maxTeams, size)
+        
+class Player:
+        def __init__(self, name, team, playerId, email, socialMedia, dateOfBirth):
+
+class Person:
+        def __init__(self, name, dateOfBirth):
+                pass
--- models/tournament.py
+++
@@ -1,5 +0,0 @@
-from datetime import date 
-
-class Tournament:
-        def __init__(self, name, startDate, endDate, venue, contactPerson, teams, matches):
-                pass 
