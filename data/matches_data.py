47d4d461408c6df030ab48d00d7f556ea139b357
Thorsteinnk92 <thorsteinnk92@gmail.com>
2025-12-03 13:29:20 +0000

    data match change

--- data/matches_data.py
+++ data/matches_data.py
@@ -5,8 +5,18 @@ class MatchesData:
         with open ("matches.csv", mode = "r", encoding = "utf-8") as file:
             reader: csv.DictReader (file)
             for line in reader:
-                menu_data: str = line.get("date", "")
-                menu_data: str = line.get("date", "")
-                menu_data: str = line.get("date", "")
+                match_data: str = line.get("match_id", "")
+                match_data: str = line.get("tournament_id", "")
+                match_data: str = line.get("round", "")
+                match_data: str = line.get("match_number", "")
+                match_data: str = line.get("team_a_name", "")
+                match_data: str = line.get("team_b_name", "")
+                match_data: str = line.get("match_date", "")
+                match_data: str = line.get("match_time", "")
+                match_data: str = line.get("server_id", "")
+                match_data: str = line.get("score_a", "")
+                match_data: str = line.get("score_b", "")
+                match_data: str = line.get("winner_team_name", "")
+                match_data: str = line.get("completed", "")
 
 
