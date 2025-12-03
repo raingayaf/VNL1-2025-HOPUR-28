58d7555f5eacc4a3a46a2cfc74c05de0b4fc3131
katrinlif95 <katrinlif95@gmail.com>
2025-12-03 16:33:39 +0000

    Lagfæringar og fleiri breytingar á UI

--- ui/dummy.py
+++ ui/dummy.py
@@ -4,16 +4,12 @@ def main_menu():
     """Aðal valmyndin."""
     print('*' * WIDTH)
     print('E-SPORTS'.center(WIDTH))                                                          
-    print('*' * WIDTH)
-    print()
-    print('Velkomin'.center(WIDTH))
-    print()
+    print('*' * WIDTH + '\n')
+    print('Velkomin'.center(WIDTH) + '\n')
     print(' 1. Notandi')
     print(' 2. Fyrirliði')
-    print(' 3. Mótshaldari')
-    print()
-    print('*' * WIDTH)
-    print()
+    print(' 3. Mótshaldari' + '\n')
+    print('*' * WIDTH + '\n')
     # user_input = input('Sláðu inn númer aðgerðar: ')
 
 # main_menu() 
@@ -24,34 +20,27 @@ def menu_user():
     """Fyrsta valmyndin hjá notanda."""
     print('*' * WIDTH)
     print('E-SPORTS'.center(WIDTH))                                                          
-    print('*' * WIDTH)
-    print()
-    print('Mót'.center(WIDTH))
-    print()
-    print(' 1. XXXX') # XXXX á að vera heiti á liði
-    print()
-    print('*' * WIDTH)
-    print()
+    print('*' * WIDTH + '\n')
+    print('Mót'.center(WIDTH) + '\n')
+    print(' 1. XXXX' + '\n') # XXXX á að vera heiti á móti
+    print('*' * WIDTH + '\n')
     # user_input = input('Sláðu inn númer móts: ')
 
 # menu_user() 
-# #--> Ef þið viljið sjá hvernig þetta prentast út 
+#--> Ef þið viljið sjá hvernig þetta prentast út 
 
 
 def menu_tournament():
+    """Notandi hefur valið mótið sem hann vill skoða og getur valið um að skoða dagskrá, stöðutöflu og keppnislið."""
     print('*' * WIDTH)
     print('E-SPORTS'.center(WIDTH))                                                          
-    print('*' * WIDTH)
-    print()
-    print('XXXX'.center(WIDTH))         # XXXX á að vera heiti á liði
-    print()
-    print('1. Dagskrá')
-    print('2. Stöðutafla')
-    print('3. Keppnislið')
-    print()
-    print('************************************')
-    print()
-    # user_input = input('Sláðu inn númer móts: ')
+    print('*' * WIDTH + '\n')
+    print('XXXX'.center(WIDTH) + '\n')         # XXXX á að vera heiti á móti
+    print(' 1. Dagskrá')
+    print(' 2. Stöðutafla')
+    print(' 3. Keppnislið' + '\n')
+    print('*' * WIDTH + '\n')
+    # user_input = input('Sláðu inn númer aðgerðar: ')
 
 # menu_tournament() 
 # #--> Ef þið viljið sjá hvernig þetta prentast út
@@ -60,15 +49,21 @@ def menu_tournament():
 def menu_organizer():
     print('*' * WIDTH)
     print('E-SPORTS'.center(WIDTH))                                                          
-    print('*' * WIDTH)
-    print()
+    print('*' * WIDTH + '\n')
+    print('Velkominn mótshaldari'.center(WIDTH) + '\n')
+    print(' 1. Skrá mót')
+    print(' 2. Útfæra mótsdagskrá')
+    print(' 3. Upplýsingar um lið' + '\n')
+    print('*' * WIDTH + '\n')
+    # user_input = input('Sláðu inn númer aðgerðar: ')
+
+
 
 
 def menu_team_captain():
     print('*' * WIDTH)
     print('E-SPORTS'.center(WIDTH))                                                          
-    print('*' * WIDTH)
-    print()
+    print('*' * WIDTH + '\n')
                                            
                                  
 
