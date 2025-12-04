91a3f907e2d961b8a59e49c95927a842c6de09fb
katrinlif95 <katrinlif95@gmail.com>
2025-12-04 13:24:01 +0000

    enn fleiri breytingar á UI

--- ui/main_menu_ui.py
+++ ui/main_menu_ui.py
@@ -1,2 +1,19 @@
 class MainMenuUI:
-    pass
+    
+    WIDTH = 60
+
+    def display_main_menu(self): 
+        """Display main menu options to user."""
+        print('*' * self.WIDTH)
+        print('E-SPORTS'.center(self.WIDTH))                                                          
+        print('*' * self.WIDTH + '\n')
+        print('Velkomin'.center(self.WIDTH) + '\n')
+        print(' 1. Notandi')
+        print(' 2. Fyrirliði')
+        print(' 3. Mótshaldari' + '\n')
+        print('*' * self.WIDTH + '\n')
+        # user_input = input('Sláðu inn númer aðgerðar: ')
+
+# main_menu = MainMenuUI()
+# main_menu.display_main_menu()
+# --> Ef þið viljið sjá hvernig þetta prentast út 
