8d9746e051b97bbace2f8a4c5a7b25a13cf52426
Thorsteinnk92 <thorsteinnk92@gmail.com>
2025-12-05 09:18:42 +0000

    breytingar a modelMatches

--- models/modelMatch.py
+++ models/modelMatch.py
@@ -1,14 +1,19 @@
-
+from datetime import date
 
 class Match:
     """Represents a match between two teams"""
 
-    def __init__(self, match_id: int, team_1: Team, team_2: Team, match_date: datetime, match_time: int, server_id: str, result: Optional[str] = None, completed = bool):
-        self.match_id = match_id              
-        self.team_1 = team_1                  
-        self.team_2 = team_2                
-        self.match_date = match_date 
-        self.match_time = match_time 
-        self.server_id = server_id           
-        self.result = result
-        self.completed = completed  
+    def __init__(self, match_id: int, tournament_id: str,
+team_a_name: Team, team_b_name: Team, match_date: date, match_time: int, server_id: str, score_a: int, score_b: int, winner_team_name: Team, completed = bool):
+
+        self.match_id = match_id
+        self.tournament_id = tournament_id
+        self.team_a_name = team_a_name
+        self.team_b_name = team_b_name
+        self.match_date = match_date
+        self.match_time = match_time
+        self.server_id = server_id
+        self.score_a = score_a
+        self.score_b = score_b
+        self.winner_team_name = winner_team_name
+        self.completed = completed
