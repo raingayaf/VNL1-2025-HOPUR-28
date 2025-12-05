74b49318a49383b747793e9292b3282b92225097
Thorsteinnk92 <thorsteinnk92@gmail.com>
2025-12-05 10:23:35 +0000

    vinna í LLApi

--- models/modelPerson.py
+++ models/modelPerson.py
@@ -2,6 +2,8 @@ from datetime import date
 
 
 class Person:
-    def __init__(self, name: str, date_of_birth: date):
+    def __init__(self, name: str, date_of_birth: date, phone: str, email: str):
         self.name = name
         self.date_of_birth = date_of_birth
+        self.email = email
+        self.phone = phone
