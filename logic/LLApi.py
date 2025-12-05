4c834ecd43f336f5a55927c5bb72f35053caca63
Thorsteinnk92 <thorsteinnk92@gmail.com>
2025-12-05 11:13:00 +0000

    lagaði vitleysu í LLApi

--- logic/LLApi.py
+++ logic/LLApi.py
@@ -8,43 +8,9 @@ from models.modelPerson import Person
 class LogicApi:
     """LogicApi lætur Logic layer og UI layer tala saman"""
     def __init__(self):
-        self._tournaments: list[Tournament] = []   
-
-    def create_tournament(self, name: str, start: date, end: date, venue: str,
-contact: Person, max_servers: int) -> Tournament:
-
-        new_id = len(self._tournaments) + 1 #Ef við viljum fara í B kröfuna(Eyða fyrir skil annars)
-
-        tournament = Tournament(
-            tournament_id = new_id,
-            name = name,
-            venue = venue,
-            start_date = start,
-            end_date = end,
-            contact_name = contact.name,
-            contact_email = contact.email,
-            contact_phone = contact.phone,
-            max_servers = max_servers
-        )
-
-        self._tournaments.append(tournament)
-        return tournament
+        #Listi yfir þær keppnir sem eru í gangi
+        self._tournaments: list[Tournament] = []
+        #Tekur tournament_id og býr til lista af Team objects
+        self._tournament_teams: dict[int, list[Team]] = {}
 
 
-    def add_team_to_tournament(self, tournament_id: int, team: Team) -> bool:
-        pass
-
-    def create_match(self, team_1: Team, team_2: Team, time: int) -> Match:
-        pass
-
-    def register_player(self, player: Player) -> bool:
-        pass
-
-    def archive_matches(self, match_id: int, winner: Team):
-        pass
-
-    def list_tournaments(self) -> list[Tournament]:
-        pass
-
-    def list_matches(self) -> list[Match]:
-        pass
