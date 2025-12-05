120c5b8ff9688748f6ca4e2511a9492b4b26f0da
kalli-krull <karlanton1997@gmail.com>
2025-12-05 14:53:46 +0000

    bætt við nokkrum exceptions

--- models/excpetions.py
+++ models/excpetions.py
@@ -14,3 +14,27 @@ class MenuNotFoundError(DataAccessError):
     """Raised when a specific menu cannot be found."""
 
     pass
+
+
+class FormatError(ValidationError):
+    """Raised when specific input is in wrong format(email, date, etc.)"""
+
+    pass
+
+
+class OutOfRangeError(ValidationError):
+    """Raised when input requires specific range but input is outside allowed limits"""
+
+    pass
+
+
+class DuplicateEntryError(ValidationError):
+    """Raised when something already exists and must be unique"""
+
+    pass
+
+
+class PermissionDeniedError(DataAccessError):
+    """raised when user is not allowed access to resource"""
+
+    pass
