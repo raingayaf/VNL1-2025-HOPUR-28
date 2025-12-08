2abe0e2305c2793a43ce1b0dbacd4694d5e38d52
katrinlif95 <katrinlif95@gmail.com>
2025-12-08 22:55:21 +0000

    Fínpússun á kóða, kommentum og docstrengjum.

--- logic/tournament_logic.py
+++ logic/tournament_logic.py
@@ -1,27 +1,24 @@
 from models.model_tournament import Tournament
 from data.data_api import DataApi
 
-
 class TournamentLogic:
-    """Logic layer for tournaments."""
+    """Logic for accessing and managing tournament data."""
     def __init__(self, data_api: DataApi):
-        """ """
+        """Initializes the logic class with refrence to the data API."""
         self._data_api = data_api
     
     def get_all_tournaments(self) -> list[Tournament]:
-        """ """
+        """Retrieves a list of all tournaments from the data layer."""
         return self._data_api.read_all_tournaments()
 
     def get_tournament_name_list(self) -> list[str]:
-        """ """
+        """Returns a list of tournament names."""
         tournaments = self.get_all_tournaments()
-        return [t.name for t in tournaments]
+        return [tournament.name for tournament in tournaments]
     
     def get_tournament_by_index(self, index: int) -> Tournament:
-        """ """
+        """Returns the tournament at the given index."""
         tournaments = self.get_all_tournaments()
-        if index < 0 or index >= len(tournaments):
-            raise IndexError('...')
         return tournaments[index]
     
 
